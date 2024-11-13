36764a4a3f75c85553304375312a1eaa38f829b1
Matt Morris <mmorris515@gmail.com>
2024-11-13 14:48:10 -0800

    Add rethink sans font

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,21 @@
 module.exports = {
   content: ["./src/**/*.tsx", "./src/**/*.ts"],
   theme: {
-    extend: {},
+    extend: {
+      container: {
+        padding: {
+          DEFAULT: "2rem",
+          sm: "1rem",
+          lg: "6rem",
+          xl: "8rem",
+          "2xl": "10rem",
+        },
+        center: true,
+      },
+      maxWidth: { "7xl": "80rem" },
+      fontFamily: {
+        custom: ["Rethink Sans", "sans-serif"],
+      },
+    },
   },
-  plugins: [],
 };
