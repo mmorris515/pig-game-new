21dc4b97e94a1f96dd5ddb9f7bb719c88edd16a6
Matt Morris <mmorris515@gmail.com>
2024-12-18 11:21:34 -0800

    Update links and icon size on header

--- src/components/Layout/Header.tsx
+++ src/components/Layout/Header.tsx
@@ -1,12 +1,21 @@
-// src/components/Layout/Header.tsx
 import React from "react";
 import PigIcon from "./PigIcon";
+import { Link } from "react-router-dom";
 
 const Header: React.FC = () => {
   return (
-    <header className="bg-red-600 text-white p-4 flex items-center justify-between shadow-md">
-      <PigIcon />
-      <h1 className="text-2xl font-bold">Two Dice Pig</h1>
+    <header className="bg-red-600 text-white p-2 flex items-center justify-between shadow-md">
+      <div className="flex items-center">
+        <PigIcon />
+      </div>
+      <div className="flex-1 flex justify-center">
+        <Link to="/">
+          <h1 className="text-2xl font-bold hover:drop-shadow-[0_4px_6px_rgba(255,255,255,0.5)] active:drop-shadow-[0_4px_6px_rgba(255,255,255,0.5)] transition-shadow duration-300">
+            Two Dice Pig
+          </h1>
+        </Link>
+      </div>
+      <div className="w-12"></div>
     </header>
   );
 };
