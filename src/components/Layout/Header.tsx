5952e8f387025d5b7bba70524c4c39e893a1b839
Matt Morris <mmorris515@gmail.com>
2025-01-10 10:17:24 -0800

    Increase header size

--- src/components/Layout/Header.tsx
+++ src/components/Layout/Header.tsx
@@ -4,13 +4,13 @@ import { Link } from "react-router-dom";
 
 const Header: React.FC = () => {
   return (
-    <header className="bg-red-600 text-white p-2 flex items-center justify-between shadow-md">
+    <header className="bg-red-600 text-white p-3 flex items-center justify-between shadow-md">
       <div className="flex items-center">
         <PigIcon />
       </div>
       <div className="flex-1 flex justify-center">
         <Link to="/">
-          <h1 className="text-2xl font-bold hover:drop-shadow-[0_4px_6px_rgba(255,255,255,0.5)] active:drop-shadow-[0_4px_6px_rgba(255,255,255,0.5)] transition-shadow duration-300">
+          <h1 className="text-4xl font-bold hover:drop-shadow-[0_4px_6px_rgba(255,255,255,0.5)] active:drop-shadow-[0_4px_6px_rgba(255,255,255,0.5)] transition-shadow duration-300">
             Two Dice Pig
           </h1>
         </Link>
