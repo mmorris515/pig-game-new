95d09f915ebb5d213640a07669e9217a630d9b6e
Matt Morris <mmorris515@gmail.com>
2025-01-14 15:50:16 -0800

    Adjust text size at small dimensions

--- src/components/Layout/Header.tsx
+++ src/components/Layout/Header.tsx
@@ -10,7 +10,7 @@ const Header: React.FC = () => {
       </div>
       <div className="flex-1 flex justify-center">
         <Link to="/" title="Homepage and rules">
-          <h1 className="text-4xl font-bold hover:drop-shadow-[0_4px_6px_rgba(255,255,255,0.5)] active:drop-shadow-[0_4px_6px_rgba(255,255,255,0.5)] transition-shadow duration-300">
+          <h1 className="text-2xl md:text-4xl font-bold hover:drop-shadow-[0_4px_6px_rgba(255,255,255,0.5)] active:drop-shadow-[0_4px_6px_rgba(255,255,255,0.5)] transition-shadow duration-300">
             Two Dice Pig
           </h1>
         </Link>
